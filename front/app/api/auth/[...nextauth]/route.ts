28d16d1c4bfce44afc06e7afdb888252c6e5e9cb
flo-ipssi <florian.emmanuel@outlook.fr>
2024-07-31 22:17:00 +0200

    Fix - Update Next + Pages auth and subscribe

---
+++ front/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    session: {
+      strategy: "jwt",
+    },
+  
+    pages: {
+      signIn: "/login",
+    },
+  
+    providers: [
+      CredentialsProvider({
+        // The name to display on the sign in form (e.g. 'Sign in with...')
+        name: "Credentials",
+        // The credentials is used to generate a suitable form on the sign in page.
+        // You can specify whatever fields you are expecting to be submitted.
+        // e.g. domain, username, password, 2FA token, etc.
+        // You can pass any HTML attribute to the <input> tag through the object.
+        credentials: {
+          email: {},
+          password: {},
+        },
+        async authorize(credentials, req) {
+          const res = await axios.post<LoginResponse>(
+            process.env.API_BASE_URL + "api/v1/auth/login",
+            credentials,
+            {
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          if (res.status === 200) {
+            const { name, id, phone, role, picture } = res.data.user;
+            const user = {
+              id,
+              name,
+              phone,
+              role,
+              picture,
+              token: res.data.token,
+            };
+            return user; // User interface we declared in next-auth.d.ts
+          } else throw new Error("Login failed");
+        },
+      }),
+    ],
+  });
+
+export { handler as GET,   handler as POST}
